6ffd09c78c59372a710ef3f280761cb8db50cd58
JeffGreenlee42git config --global user.name JeffGreenlee42git config --global user.name JeffGreenlee42 <banjo1@gmail.com>
2018-06-20 06:42:16 -0700

    Added add.Suggestion code

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -5,4 +5,19 @@ app.controller('HomeController',
      function($scope, suggestions) {
         $scope.helloWorld = "Hello, AngularJS!";
         $scope.posts = suggestions.posts;
-    }]);
+     },
+
+    $scope.addSuggestion = function() {
+        //if the input data is empty,  exit this function
+        if (!$scope.title || $scope.title === '') {
+            return;
+        }
+        // add input suggestion to suggestions array
+        $scope.posts.push({
+            title: $scope.title,
+            upVotes: 0,
+        });
+        // after adding suggestion, Clear the input
+        $scope.title = '';
+    },
+])
