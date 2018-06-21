c9f30c571847dc8849f43b7ad23237ca200d33a3
JeffGreenlee42git config --global user.name JeffGreenlee42git config --global user.name JeffGreenlee42 <banjo1@gmail.com>
2018-06-20 19:13:28 -0700

    Full function
    Main Contrtoller all functions working.

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -5,19 +5,20 @@ app.controller('HomeController',
      function($scope, suggestions) {
         $scope.helloWorld = "Hello, AngularJS!";
         $scope.posts = suggestions.posts;
-     },
-
-    $scope.addSuggestion = function() {
-        //if the input data is empty,  exit this function
-        if (!$scope.title || $scope.title === '') {
-            return;
-        }
-        // add input suggestion to suggestions array
-        $scope.posts.push({
-            title: $scope.title,
-            upVotes: 0,
-        });
-        // after adding suggestion, Clear the input
-        $scope.title = '';
-    },
-])
+        $scope.addSuggestion = function() {
+            //if the input data is empty,  exit this function
+            if (!$scope.title || $scope.title === "") {
+                return;
+            }
+            // add input suggestion to suggestions array
+            $scope.posts.push({
+                title: $scope.title,
+                upvotes: 0,
+            });
+            // after adding suggestion, Clear the input
+            $scope.title = '';
+        };
+        $scope.upVote = function(post) {
+            post.upvotes += 1;
+        };
+     }])
